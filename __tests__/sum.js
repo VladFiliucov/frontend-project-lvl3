963c6df10f9846fe40f462f00d007bae69977068
Vlad Filiucov <filiucov@mail.ru>
2021-02-26 13:57:31 +0000

    initial jest setup wip

---
+++ __tests__/sum.js
@@ -0,0 +1,7 @@
+import sum from './src/index.js';
+
+describe('sum', () => {
+  it('works', () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,4 @@
+export default {
+  testEnvironment: 'jest-environment-node',
+  testPathIgnorePatterns: ['/node_modules/', '/__tests__/__fixtures__/'],
+};
