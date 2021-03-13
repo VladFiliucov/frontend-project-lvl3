3207bb4420a229f2d065ce9ee8db743d25276011
Vlad Filiucov <filiucov@mail.ru>
2021-03-13 12:49:30 +0000

    fetch correct feed from the url

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import * as yup from 'yup';
 import parseResponse from './parseResponse.js';
 import watchState from './view/index.js';
 import 'regenerator-runtime/runtime.js'; // https://github.com/babel/babel/issues/9849#issuecomment-487040428
-import * as yup from 'yup';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const fetchFeed = (url) => (
+  fetch(url)
+    .then((response) => response.text())
+    .then((str) => (new window.DOMParser()).parseFromString(str, 'text/xml'))
+);
 
 const app = () => {
   const state = {
@@ -14,23 +20,23 @@ const app = () => {
       errors: {},
     },
     feeds: [{
-      url: 'https://ru.hexlet.io/lessons.rss',
+      url: 'https://ru.hexlet.io/lessons.rsss',
       title: 'foo',
       description: 'bar',
       items: [
         {
           title: 'Рациональные числа',
           link: 'https://ru.hexlet.io/courses/ruby-compound-data/lessons/rational/theory_unit',
-          description: 'Цель: Рассмотреть рациональные числа как новый пример абстракции на основе пар чисел.'
-        }
-      ]
+          description: 'Цель: Рассмотреть рациональные числа как новый пример абстракции на основе пар чисел.',
+        },
+      ],
     }],
   };
 
   const watchedState = watchState(state);
 
   const schema = yup.object().shape({
-    feedUrl: yup.string().required().url().notOneOf(state.feeds.map(feed => feed.url), 'RSS уже существует'),
+    feedUrl: yup.string().required().url().notOneOf(state.feeds.map((feed) => feed.url), 'RSS уже существует'),
   });
 
   const validateForm = (formData) => schema.validate(formData);
@@ -41,19 +47,19 @@ const app = () => {
     event.preventDefault();
     // validation on submit
     const formData = new FormData(event.target);
-    validateForm({ feedUrl: formData.get('url') })
+    const url = formData.get('url');
+    validateForm({ feedUrl: url })
       .then(() => {
-        fetch('https://ru.hexlet.io/lessons.rss')
-          .then((response) => response.text())
-          .then((str) => (new window.DOMParser()).parseFromString(str, 'text/xml'))
-          .then((xmlDoc) => console.log(parseResponse(xmlDoc)));
-        watchedState.feeds.push({url: 'Hithere'})
-        console.log("shouldnt bee here if validation failed");
+        fetchFeed(url)
+          .then((xmlDoc) => {
+            const parsedFeed = parseResponse(xmlDoc);
+            watchedState.feeds.push({ url, ...parsedFeed });
+          });
       })
       .catch((err) => {
         console.log(err.name); // => 'ValidationError'
         console.log(err.errors);
-      })
+      });
   });
 };
 
