384cdfeb7723ccb3d5bc59493d6a79eaccc4dc10
Vlad Filiucov <filiucov@mail.ru>
2021-04-05 19:58:41 +0100

    swap i18 around

--- src/index.js
+++ src/index.js
@@ -8,107 +8,108 @@ import watchState from './view/index.js';
 import 'regenerator-runtime/runtime.js'; // https://github.com/babel/babel/issues/9849#issuecomment-487040428
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const app = (t) => {
-  const state = {
-    form: {
-      status: FORM_STATES.pending,
-      url: '',
-      valid: false,
-      errors: [],
-    },
-    feeds: [],
-    posts: [],
-    activePost: null,
-    visitedPostIds: [],
-    processing: false,
-  };
+const app = () => {
+  localePromise().then((t) => {
+    const state = {
+      form: {
+        status: FORM_STATES.pending,
+        url: '',
+        valid: false,
+        errors: [],
+      },
+      feeds: [],
+      posts: [],
+      activePost: null,
+      visitedPostIds: [],
+      processing: false,
+    };
 
-  yup.setLocale({
-    string: {
-      url: 'urlInvalid',
-    },
-    mixed: {
-      notOneOf: 'existingRSS',
-    },
-  });
+    yup.setLocale({
+      string: {
+        url: 'urlInvalid',
+      },
+      mixed: {
+        notOneOf: 'existingRSS',
+      },
+    });
 
-  const form = document.querySelector('#rss-form');
-  const feedback = document.querySelector('div.feedback');
-  const submitButton = form.querySelector('button');
-  const input = form.querySelector('input');
-  const postsNode = document.querySelector('.posts');
-  const activePostModal = document.querySelector('#activePostModal');
+    const form = document.querySelector('#rss-form');
+    const feedback = document.querySelector('div.feedback');
+    const submitButton = form.querySelector('button');
+    const input = form.querySelector('input');
+    const postsNode = document.querySelector('.posts');
+    const activePostModal = document.querySelector('#activePostModal');
 
-  const selectors = {
-    postsNode,
-    formElements: {
-      form, submitButton, input, feedback,
-    },
-  };
+    const selectors = {
+      postsNode,
+      formElements: {
+        form, submitButton, input, feedback,
+      },
+    };
 
-  const watchedState = watchState(state, t, selectors);
+    const watchedState = watchState(state, t, selectors);
 
-  const schema = yup.string().required().url();
+    const schema = yup.string().required().url();
 
-  const getValidationErrors = (feedUrl) => {
-    try {
-      schema
-        .notOneOf(watchedState.feeds.map((feed) => feed.url))
-        .validateSync(feedUrl);
-      return [];
-    } catch (err) {
-      return err.errors;
-    }
-  };
+    const getValidationErrors = (feedUrl) => {
+      try {
+        schema
+          .notOneOf(watchedState.feeds.map((feed) => feed.url))
+          .validateSync(feedUrl);
+        return [];
+      } catch (err) {
+        return err.errors;
+      }
+    };
 
-  activePostModal.addEventListener('hidden.bs.modal', () => {
-    watchedState.activePost = null;
-  });
+    activePostModal.addEventListener('hidden.bs.modal', () => {
+      watchedState.activePost = null;
+    });
 
-  postsNode.addEventListener('click', (e) => {
-    if (e.target.dataset.id) {
-      const activePost = watchedState.posts.find((post) => post.id === e.target.dataset.id);
-      watchedState.activePost = activePost;
-      watchedState.visitedPostIds.push(activePost.id);
-    }
-  });
+    postsNode.addEventListener('click', (e) => {
+      if (e.target.dataset.id) {
+        const activePost = watchedState.posts.find((post) => post.id === e.target.dataset.id);
+        watchedState.activePost = activePost;
+        watchedState.visitedPostIds.push(activePost.id);
+      }
+    });
 
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    watchedState.processing = true;
-    const formData = new FormData(event.target);
-    const url = formData.get('url');
-    watchedState.form.errors = getValidationErrors(url);
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      watchedState.processing = true;
+      const formData = new FormData(event.target);
+      const url = formData.get('url');
+      watchedState.form.errors = getValidationErrors(url);
 
-    if (watchedState.form.errors.length > 0) {
-      watchedState.processing = false;
-      return;
-    }
-
-    fetchFeed(url)
-      .then(({ data }) => {
-        const parsedFeed = parseFeed(data.contents);
-        watchedState.feeds.push({ url, ...parsedFeed.feed });
-        const newPosts = parsedFeed.posts.map((post) => ({ feedId: url, id: post.link, ...post }));
-        watchedState.posts = [...newPosts, ...state.posts];
-        watchedState.form.status = FORM_STATES.success;
-      })
-      .catch((error) => {
-        if (error.isAxiosError) {
-          watchedState.form.errors = ['networkError'];
-        } else {
-          watchedState.form.errors = ['parsingError'];
-        }
-      })
-      .finally(() => {
+      if (watchedState.form.errors.length > 0) {
         watchedState.processing = false;
-        watchedState.form.status = FORM_STATES.pending;
-      });
-  });
+        return;
+      }
 
-  observeFeedsUpdates(watchedState);
+      fetchFeed(url)
+        .then(({ data }) => {
+          const parsedFeed = parseFeed(data.contents);
+          watchedState.feeds.push({ url, ...parsedFeed.feed });
+          const newPosts = parsedFeed
+            .posts.map((post) => ({ feedId: url, id: post.link, ...post }));
+          watchedState.posts = [...newPosts, ...state.posts];
+          watchedState.form.status = FORM_STATES.success;
+        })
+        .catch((error) => {
+          if (error.isAxiosError) {
+            watchedState.form.errors = ['networkError'];
+          } else {
+            watchedState.form.errors = ['parsingError'];
+          }
+        })
+        .finally(() => {
+          watchedState.processing = false;
+          watchedState.form.status = FORM_STATES.pending;
+        });
+    });
+
+    observeFeedsUpdates(watchedState);
+  });
 };
 
-export default () => localePromise().then((text) => {
-  app(text);
-});
+export default () => app();
