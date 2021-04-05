f16f38a7c9cadbc8cbd148e72b85afd5a1bf0a46
Vlad Filiucov <filiucov@mail.ru>
2021-04-05 20:43:37 +0100

    undo

--- src/initializers/i18n.js
+++ src/initializers/i18n.js
@@ -1,44 +1,36 @@
 import i18next from 'i18next';
 
-const localePromise = () => {
-  const i18nextInstance = i18next.createInstance();
-
-  return new Promise((resolve) => {
-    resolve(i18nextInstance.init({
-      // lng: navigator.language,
-      lng: 'ru',
-      resources: {
-        'en-US': {
-          translation: {
-            success: 'RSS uploaded successfully',
-            networkError: 'Network error. Please try again',
-            urlInvalid: 'Field should be a valid URL',
-            existingRSS: 'RSS already exists',
-            parsingError: 'Could not extract RSS feed from this url',
-            preview: 'Preview',
-            posts: 'Posts',
-            feeds: 'Feeds',
-            close: 'Close',
-            readMore: 'Read more',
-          },
-        },
-        ru: {
-          translation: {
-            success: 'RSS успешно загружен',
-            networkError: 'Произошла ошибка сети. Попробуйте еще раз',
-            urlInvalid: 'Ссылка должна быть валидным URL',
-            existingRSS: 'RSS уже существует',
-            parsingError: 'Ресурс не содержит валидный RSS',
-            preview: 'Просмотр',
-            posts: 'Публикации',
-            feeds: 'Фиды',
-            close: 'Закрыть',
-            readMore: 'Читать полностью',
-          },
-        },
+export default () => i18next.init({
+  // lng: navigator.language,
+  lng: 'ru',
+  resources: {
+    'en-US': {
+      translation: {
+        success: 'RSS uploaded successfully',
+        networkError: 'Network error. Please try again',
+        urlInvalid: 'Field should be a valid URL',
+        existingRSS: 'RSS already exists',
+        parsingError: 'Could not extract RSS feed from this url',
+        preview: 'Preview',
+        posts: 'Posts',
+        feeds: 'Feeds',
+        close: 'Close',
+        readMore: 'Read more',
       },
-    }));
-  });
-};
-
-export default localePromise;
+    },
+    ru: {
+      translation: {
+        success: 'RSS успешно загружен',
+        networkError: 'Произошла ошибка сети. Попробуйте еще раз',
+        urlInvalid: 'Ссылка должна быть валидным URL',
+        existingRSS: 'RSS уже существует',
+        parsingError: 'Ресурс не содержит валидный RSS',
+        preview: 'Просмотр',
+        posts: 'Публикации',
+        feeds: 'Фиды',
+        close: 'Закрыть',
+        readMore: 'Читать полностью',
+      },
+    },
+  },
+});
