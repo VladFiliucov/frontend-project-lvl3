4dfb7ac6780c4f0db43756939f810cea4c043152
Vlad Filiucov <filiucov@mail.ru>
2021-04-14 12:21:38 +0100

    move business logic away from utils folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,65 @@
 import * as yup from 'yup';
 import i18next from 'i18next';
-import {
-  fetchFeed, observeFeedsUpdates, parseFeed,
-} from './utils/index.js';
+import differenceWith from 'lodash/differenceWith.js';
+import isEqual from 'lodash/isEqual.js';
+import fetchFeed from './utils/index.js';
 import localisationConfig from './initializers/i18n.js';
 import watchState from './view/index.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const parseFeed = (rawFeed) => {
+  const xmlDoc = (new DOMParser()).parseFromString(rawFeed, 'text/xml');
+  const parserError = xmlDoc.querySelector('parsererror');
+
+  if (parserError) {
+    throw new Error(`Parsing error: ${parserError.textContent}`);
+  }
+
+  const title = xmlDoc.querySelector('channel > title').textContent;
+  const description = xmlDoc.querySelector('channel > description').textContent;
+  const posts = [...xmlDoc.querySelectorAll('item')].map((item) => {
+    const postLink = item.querySelector('link').textContent;
+    const postTitle = item.querySelector('title').textContent;
+    const postDescription = item.querySelector('description').textContent;
+
+    return ({
+      link: postLink,
+      title: postTitle,
+      description: postDescription,
+    });
+  });
+
+  return {
+    feed: {
+      title,
+      description,
+    },
+    posts,
+  };
+};
+
+const renewFeed = (feedURL, watchedState) => fetchFeed(feedURL)
+  .then(({ data }) => {
+    const parsedFeed = parseFeed(data.contents);
+    const savedPosts = watchedState.posts.filter((post) => post.feedId === feedURL);
+    const latestPosts = parsedFeed
+      .posts.map((post) => ({ feedId: feedURL, id: post.link, ...post }));
+
+    const newPosts = differenceWith(latestPosts, savedPosts, isEqual);
+    newPosts.forEach((newPost) => watchedState.posts.unshift(newPost));
+  })
+  .catch(() => {
+    console.error("Couldn't renew feed for", feedURL);
+  });
+
+export const observeFeedsUpdates = (watchedState) => {
+  const promises = watchedState.feeds.map((feed) => renewFeed(feed.url, watchedState));
+  const refreshInterval = 5000;
+
+  Promise.allSettled(promises)
+    .then(() => setTimeout(() => observeFeedsUpdates(watchedState), refreshInterval));
+};
+
 
 const app = (t) => {
   const state = {
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,37 +1,4 @@
-import differenceWith from 'lodash/differenceWith.js';
-import isEqual from 'lodash/isEqual.js';
 import axios from 'axios';
-
-export const parseFeed = (rawFeed) => {
-  const xmlDoc = (new DOMParser()).parseFromString(rawFeed, 'text/xml');
-  const parserError = xmlDoc.querySelector('parsererror');
-
-  if (parserError) {
-    throw new Error(`Parsing error: ${parserError.textContent}`);
-  }
-
-  const title = xmlDoc.querySelector('channel > title').textContent;
-  const description = xmlDoc.querySelector('channel > description').textContent;
-  const posts = [...xmlDoc.querySelectorAll('item')].map((item) => {
-    const postLink = item.querySelector('link').textContent;
-    const postTitle = item.querySelector('title').textContent;
-    const postDescription = item.querySelector('description').textContent;
-
-    return ({
-      link: postLink,
-      title: postTitle,
-      description: postDescription,
-    });
-  });
-
-  return {
-    feed: {
-      title,
-      description,
-    },
-    posts,
-  };
-};
 
 const constructURL = (url) => {
   const fullURL = new URL('/get', 'https://hexlet-allorigins.herokuapp.com');
@@ -41,28 +8,8 @@ const constructURL = (url) => {
   return fullURL.toString();
 };
 
-export const fetchFeed = (url) => (
+const fetchFeed = (url) => (
   axios.get(constructURL(url)).then(({ data }) => ({ url, data }))
 );
 
-const renewFeed = (feedURL, watchedState) => fetchFeed(feedURL)
-  .then(({ data }) => {
-    const parsedFeed = parseFeed(data.contents);
-    const savedPosts = watchedState.posts.filter((post) => post.feedId === feedURL);
-    const latestPosts = parsedFeed
-      .posts.map((post) => ({ feedId: feedURL, id: post.link, ...post }));
-
-    const newPosts = differenceWith(latestPosts, savedPosts, isEqual);
-    newPosts.forEach((newPost) => watchedState.posts.unshift(newPost));
-  })
-  .catch(() => {
-    console.error("Couldn't renew feed for", feedURL);
-  });
-
-export const observeFeedsUpdates = (watchedState) => {
-  const promises = watchedState.feeds.map((feed) => renewFeed(feed.url, watchedState));
-  const refreshInterval = 5000;
-
-  Promise.allSettled(promises)
-    .then(() => setTimeout(() => observeFeedsUpdates(watchedState), refreshInterval));
-};
+export default fetchFeed;
